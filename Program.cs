03d3fa82455bb71d90a740b3d387ee37c2fbcd22
SemianikhiNick <62600902+SemianikhiNick@users.noreply.github.com>
2020-03-24 15:56:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrSemianikhin1
+{
+
+    class Adress
+    {
+        public string city { get; set; }
+
+        public string street { get; set; }
+
+        public int nubmerHouse { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress London = new Adress() { city = "London", street = "Kings Road", nubmerHouse = 47 };
+            Console.WriteLine("City: " + London.city + " Street: " + London.street + " NubmerHouse: " + London.nubmerHouse);
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
